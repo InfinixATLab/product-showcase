da645863b058ea063f8da9010651307fffef1458
Felipe Raulino <felipe.raaulino29@gmail.com>
2025-12-03 12:46:28 -0300

    refactor: reorganize project structure to integrate React Router and routes

--- ProductShowcase/src/App.tsx
+++ ProductShowcase/src/App.tsx
@@ -1,40 +1,16 @@
-import PokemonCard from "./components/PokemonCard";
-import { useFetch } from "./hooks/useFetch";
-import type { PokemonListResponse } from "./types/pokemons";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PokemonDetailPage from "./pages/PokemonDetailsPage";
 
-const App = () => {
-  const { data, loading, error } = useFetch<PokemonListResponse>(
-    "https://pokeapi.co/api/v2/pokemon"
-  );
-
-  if (error) {
-    return (
-      <div className="min-h-screen flex flex-col items-center">
-        <p className="text-black font-bold text-2xl">
-          Erro ao carregar os dados
-        </p>
-      </div>
-    );
-  }
-
+function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center py-6">
-      <h1 className="text-black font-bold text-4xl mt-6">Pokemon Desk</h1>
-
-      {loading && <p className="mt-12">Loading...</p>}
-
-      <div className="mt-8 mx-auto grid gap-4 grid-cols-1  sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4">
-        {data &&
-          data.results.map((item, index) => (
-            <PokemonCard
-              key={`${item.name}-${index}`}
-              name={item.name}
-              image={item.url}
-            />
-          ))}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:name" element={<PokemonDetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
