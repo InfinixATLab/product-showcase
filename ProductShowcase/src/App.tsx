2c7d7c9b2da3e7c7680b74f311b9c1a9610573c6
Felipe Raulino <felipe.raaulino29@gmail.com>
2025-12-03 12:34:08 -0300

    feat(home): display Pokémon cards with API data

--- ProductShowcase/src/App.tsx
+++ ProductShowcase/src/App.tsx
@@ -1,7 +1,38 @@
+import PokemonCard from "./components/PokemonCard";
+import { useFetch } from "./hooks/useFetch";
+import type { PokemonListResponse } from "./types/pokemons";
+
 const App = () => {
+  const { data, loading, error } = useFetch<PokemonListResponse>(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+
+  if (error) {
+    return (
+      <div className="min-h-screen flex flex-col items-center">
+        <p className="text-black font-bold text-2xl">
+          Erro ao carregar os dados
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className="min-h-screen bg-black">
-      <h1 className="text-white">Hello World!</h1>
+    <div className="min-h-screen flex flex-col items-center py-6">
+      <h1 className="text-black font-bold text-4xl mt-6">Pokemon Desk</h1>
+
+      {loading && <p className="mt-12">Loading...</p>}
+
+      <div className="mt-8 mx-auto grid gap-4 grid-cols-1  sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4">
+        {data &&
+          data.results.map((item, index) => (
+            <PokemonCard
+              key={`${item.name}-${index}`}
+              name={item.name}
+              image={item.url}
+            />
+          ))}
+      </div>
     </div>
   );
 };
