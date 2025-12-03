14a61f5d8d53c7fc7ae0adf924e2ccb199058281
alison-souza <alison.sxuzx@gmail.com>
2025-12-03 14:05:18 -0300

    Ajusta cores, centraliza favoritos, adiciona ícone de coração e estiliza campo de busca

---
+++ ProductShowcase/src/types/Pokemon.ts
@@ -0,0 +1,23 @@
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
+
+export interface PokemonDetails {
+  id: number;
+  name: string;
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  height: number;
+  weight: number;
+}
