b374092c5c9f3a818e99708becfcbd36eb51d610
Luiz Antonio <l.antonio4100@gmail.com>
2025-12-03 11:13:18 -0300

    feat: adiciona rota PokemonDetail

--- ProductShowcase/src/App.tsx
+++ ProductShowcase/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-
+import { PokemonDetail } from "./pages/PokemonDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
       </Routes>
     </Router>
   );
