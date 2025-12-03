0bb72c64de6347095650b7c9be8c1ea4073f8782
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-03 10:04:38 -0300

    feat: cria componente header

---
+++ ProductShowcase/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center items-center bg-red-600 min-h-13 w-full">
+      <p className="text-white text-3xl">Pokedéx</p>
+    </div>
+  );
+}
