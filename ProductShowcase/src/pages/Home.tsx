630ee60d9fa5d29d309f8c6753f3b8117c631e34
Luiz Antonio <l.antonio4100@gmail.com>
2025-12-03 12:12:06 -0300

    feat: filtro de busca

--- ProductShowcase/src/pages/Home.tsx
+++ ProductShowcase/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import { getPokemonList, type PokemonListItem } from "../services/pokemonService
 export const Home = () => {
   const [pokemons, setPokemons] = useState<PokemonListItem[]>([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -27,12 +28,27 @@ export const Home = () => {
     return <div className="text-center mt-10">Carregando...</div>;
   }
 
+  //Filtro de busca
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4 text-center">Pokedéx</h1>
 
+      <div className="mb-4 text-center">
+        <input
+          type="text"
+          placeholder="Buscar Pokémon"
+          className="border p-2 rounded w-full max-w-xs"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        {pokemons.map((pokemon) => {
+        {filteredPokemons.map((pokemon) => {
           const id = pokemon.url.split("/")[pokemon.url.split("/").length - 2];
           const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
