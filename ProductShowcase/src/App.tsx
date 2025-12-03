ff798ecc883735ddf3bf872a90f1295a433b9541
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-12-03 10:28:31 -0300

    feat: configura rotas e layout basico

--- ProductShowcase/src/App.tsx
+++ ProductShowcase/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import PokemonDetail from './pages/PokemonDetail';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon/:name" element={<PokemonDetail />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
