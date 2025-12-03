8c9481187d430816a2e9e86c354d0d8fdc0af473
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-03 12:57:49 -0300

    feat: implementa carregamento de página enquanto busca os dados na API

--- ProductShowcase/src/pages/Details.tsx
+++ ProductShowcase/src/pages/Details.tsx
@@ -4,16 +4,19 @@ import Header from "../components/Header";
 import { getPokemonDetails } from "../services/pokeapi";
 import { useParams } from "react-router-dom";
 import type { PokemonDetails } from "../interfaces/pokemon";
+import Loading from "../components/Loading";
 
 export default function Details() {
   const { name } = useParams<{ name: string }>();
   const [pokemon, setPokemon] = useState<PokemonDetails | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadPokemonDetails() {
       if (!name) return;
       const data = await getPokemonDetails(name);
       setPokemon(data);
+      setLoading(false);
     }
     loadPokemonDetails();
   }, [name]);
@@ -22,25 +25,31 @@ export default function Details() {
     <div className="flex flex-col justify-center items-center min-h-screen">
       <Header />
       <div className="grow grid place-items-center">
-        <div className="flex flex-col justify-center items-center bg-neutral-100 m-8 p-4 rounded-md shadow-md">
-          <p className="text-4xl">{pokemon?.name}</p>
-          <img src={pokemon?.sprites.other["official-artwork"].front_default} />
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="flex flex-col justify-center items-center bg-neutral-100 m-8 p-4 rounded-md shadow-md">
+            <p className="text-4xl">{pokemon?.name}</p>
+            <img
+              src={pokemon?.sprites.other["official-artwork"].front_default}
+            />
 
-          <div className="flex justify-center items-center gap-1">
-            <p className="font-medium">Types:</p>
-            <ul className="flex justify-center items-center gap-1">
-              {pokemon?.types.map((type) => {
-                return <li>{type.type.name}</li>;
-              })}
-            </ul>
+            <div className="flex justify-center items-center gap-1">
+              <p className="font-medium">Types:</p>
+              <ul className="flex justify-center items-center gap-1">
+                {pokemon?.types.map((type) => {
+                  return <li>{type.type.name}</li>;
+                })}
+              </ul>
+            </div>
+            <p className="flex justify-center items-center gap-1">
+              <span className="font-medium">Weight:</span> {pokemon?.weight}
+            </p>
+            <p className="flex justify-center items-center gap-1">
+              <span className="font-medium">Height:</span> {pokemon?.height}
+            </p>
           </div>
-          <p className="flex justify-center items-center gap-1">
-            <span className="font-medium">Weight:</span> {pokemon?.weight}
-          </p>
-          <p className="flex justify-center items-center gap-1">
-            <span className="font-medium">Height:</span> {pokemon?.height}
-          </p>
-        </div>
+        )}
       </div>
       <Footer />
     </div>
--- ProductShowcase/src/pages/Home.tsx
+++ ProductShowcase/src/pages/Home.tsx
@@ -4,14 +4,17 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { getAllPokemons } from "../services/pokeapi";
 import type { Pokemon } from "../interfaces/pokemon";
+import Loading from "../components/Loading";
 
 export default function Home() {
   const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadPokemons() {
       const data = await getAllPokemons();
       setPokemons(data);
+      setLoading(false);
     }
     loadPokemons();
   }, []);
@@ -20,17 +23,21 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center min-h-screen">
       <Header />
       <div className="grow w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 place-items-center gap-6 py-8 px-8">
-        {pokemons?.map((pokemon, index) => {
-          const isLast = index === pokemons.length - 1;
-          return (
-            <Card
-              image={pokemon.image}
-              name={pokemon.name}
-              key={pokemon.name}
-              isLast={isLast}
-            />
-          );
-        })}
+        {loading ? (
+          <Loading />
+        ) : (
+          pokemons?.map((pokemon, index) => {
+            const isLast = index === pokemons.length - 1;
+            return (
+              <Card
+                image={pokemon.image}
+                name={pokemon.name}
+                key={pokemon.name}
+                isLast={isLast}
+              />
+            );
+          })
+        )}
       </div>
       <Footer />
     </div>
