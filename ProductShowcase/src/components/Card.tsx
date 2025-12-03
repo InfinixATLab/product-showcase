1d0ff4bd93a977151f3a73e3cb110f7482b07820
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-03 11:56:26 -0300

    feat: adiciona responsividade para tablet

--- ProductShowcase/src/components/Card.tsx
+++ ProductShowcase/src/components/Card.tsx
@@ -7,11 +7,11 @@ interface CardProps {
 export default function Card({ name, image, isLast }: CardProps) {
   return (
     <div
-      className={`bg-neutral-100 rounded-md shadow-md grid place-items-center p-2 ${
+      className={`bg-neutral-100 rounded-md shadow-md grid place-items-center p-2 md:gap-2 ${
         isLast && "col-span-full"
       }`}
     >
-      <img src={image} />
+      <img src={image} className="md:h-image-tablet" />
       <p>{name}</p>
     </div>
   );
