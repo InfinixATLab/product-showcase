c7b2451d1a7dfabb380d46620ad528a06ccbbf59
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-12-03 12:56:28 -0300

    chore: corrige config. do Vite e Tailwind

--- ProductShowcase/vite.config.ts
+++ ProductShowcase/vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-server: {
-  watch: {
-    usePolling: true,
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    port: 5173,
+    host: true,
   },
-  port: 5173,
-  host: true,
-},
   plugins: [
     react(),
-    tailwindcss(),
   ],
 })
