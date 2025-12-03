d65e44a7cfefea81c6338af092e2a3a311d09768
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-03 11:43:32 -0300

    feat: cria base da tela home

--- ProductShowcase/src/App.tsx
+++ ProductShowcase/src/App.tsx
@@ -1,3 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 export default function App() {
-  return <div>Olá Mundo</div>;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <Header />
+      <div className="grow w-full grid grid-cols-2 place-items-center"></div>
+      <Footer />
+    </div>
+  );
 }
