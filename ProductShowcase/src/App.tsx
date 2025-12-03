6db4321046349d4f0c0423c01a2a03a76445e849
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-03 11:46:49 -0300

    feat: cria cards dos 151 pokemons na tela inicial

--- ProductShowcase/src/App.tsx
+++ ProductShowcase/src/App.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { getAllPokemons } from "./services/pokeapi";
+import type { Pokemon } from "./interfaces/Pokemon";
 
 export default function App() {
+  const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
+
+  useEffect(() => {
+    async function loadPokemons() {
+      const data = await getAllPokemons();
+      setPokemons(data);
+    }
+    loadPokemons();
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <Header />
-      <div className="grow w-full grid grid-cols-2 place-items-center"></div>
+      <div className="grow w-full grid grid-cols-2 place-items-center gap-6 py-8 px-8">
+        {pokemons?.map((pokemon, index) => {
+          const isLast = index === pokemons.length - 1;
+          return (
+            <Card
+              image={pokemon.image}
+              name={pokemon.name}
+              key={pokemon.name}
+              isLast={isLast}
+            />
+          );
+        })}
+      </div>
       <Footer />
     </div>
   );
