f932d7399c254782c903129f2adf3164ec0ec143
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-03 10:14:35 -0300

    feat: cria componente footer

---
+++ ProductShowcase/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center items-center w-full min-h-9 bg-neutral-800">
+      <p className="text-white text-sm">© 2025 Nathan - Desafio técnico.</p>
+    </div>
+  );
+}
