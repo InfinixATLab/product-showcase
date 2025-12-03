471eaab634de3928c29c2819ba4be0415e5d339a
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-12-03 10:37:19 -0300

    feat: add tipos e interfaces

---
+++ ProductShowcase/src/types/pokemon.ts
@@ -0,0 +1,42 @@
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
+
+export interface PokemonListResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonListItem[];
+}
+
+export interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+    other: {
+      'official-artwork': {
+        front_default: string;
+      };
+    };
+  };
+  types: PokemonType[];
+  height: number;
+  weight: number;
+}
+
+export interface PokemonContextType {
+  favorites: string[];
+  addFavorite: (name: string) => void;
+  removeFavorite: (name: string) => void;
+  isFavorite: (name: string) => boolean;
+}
