ce47264a4a9a7cd23001c4a063bb470b622b8e92
Luiz Antonio <l.antonio4100@gmail.com>
2025-12-03 11:06:23 -0300

    feat: implementa fetch da lista de 151 pokemons e loading

---
+++ ProductShowcase/src/pages/Home.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getPokemonList, type PokemonListItem } from "../services/pokemonService";
+
+export const Home = () => {
+  const [pokemons, setPokemons] = useState<PokemonListItem[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        setLoading(true);
+        const list = await getPokemonList();
+        setPokemons(list);
+      } catch (err) {
+        console.log("Erro ao buscar Pokémon", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
+  //Tela de Carregando
+  if (loading) {
+    return <div className="text-center mt-10">Carregando...</div>;
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Pokedéx</h1>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        {pokemons.map((pokemon) => {
+          const id = pokemon.url.split("/")[pokemon.url.split("/").length - 2];
+          const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+          return (
+            <Link
+              to={`/pokemon/${pokemon.name}`}
+              key={pokemon.name}
+              className="border p-2 rounded hover:shadow-lg flex flex-col items-center"
+            >
+              <img src={imageUrl} alt={pokemon.name} className="w-20 h-20" />
+              <span className="capitalize mt-2">{pokemon.name}</span>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
